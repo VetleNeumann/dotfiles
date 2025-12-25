f0e769572b1e1b10d022438473297f3910a785b9
VetleNeumann <vetle.neumann@protonmail.com>
2025-12-25 02:21:31 +0100

    Fix pathings and scripts

--- home/.chezmoiscripts/run_onchange_before_install-aru.sh
+++ home/.chezmoiscripts/run_onchange_before_install-aru.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 sudo pacman -S --needed base-devel
 temp_directory="$(mktemp -d)"
-cd "$temp_directory"
+cd "${temp_directory}"
 git clone https://aur.archlinux.org/paru.git
-cd "paru"
+cd paru
 makepkg -si
